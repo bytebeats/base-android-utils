5d99620f7cc75d77c7f4bab9ef5dff960377c3ef
Peter Pan <happychinapc@gmail.com>
2015-01-04 09:35:32 +0800

    Create ConfigureEncryptAndDecrypt.java

---
+++ src/me/pc/mobile/helper/v14/crypt/ConfigureEncryptAndDecrypt.java
@@ -0,0 +1,8 @@
+package me.pc.mobile.helper.v14.crypt;
+
+public class ConfigureEncryptAndDecrypt {
+	public static final String CHAR_ENCODING = "UTF-8";
+	public static final String AES_ALGORITHM = "AES/ECB/PKCS5Padding";
+	public static final String RSA_ALGORITHM = "RSA/ECB/PKCS1Padding";
+	public static final String CIPHER_ALGORITHM = "AES/ECB/PKCS7Padding"; // "算法/模式/补码方式"
+}
